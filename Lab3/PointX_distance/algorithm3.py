2bbe11616b08e9881a0e2eeccccb81e7230ec2fa
konprok <konradprokop20@gmail.com>
2024-11-14 23:20:21 +0100

    PointX_distance fix?

--- Lab3/PointX_distance/algorithm3.py
+++ Lab3/PointX_distance/algorithm3.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.optimize import minimize
-from scipy.interpolate import splprep, splev, griddata
+from scipy.interpolate import splprep, splev
 
 # Definicje funkcji celu dla problemu dwukryterialnego
 def F1(x, y):
@@ -20,41 +20,51 @@ def G2(x, y, z):
 def G3(x, y, z):
     return x * y * z  # Nieliniowa funkcja iloczynowa
 
-# Funkcja skalaryzowana dla problemu dwukryterialnego (minimalizacja odległości)
-def distance_scalarization_F(x, target_point, p=2):
+# Funkcja skalaryzowana dla problemu dwukryterialnego z uwzględnieniem wag
+def distance_scalarization_F(x, target_point, lambda_weights, p=2):
     F_values = np.array([F1(x[0], x[1]), F2(x[0], x[1])])
-    return np.linalg.norm(F_values - target_point, ord=p)
+    scaled_diff = lambda_weights * (F_values - target_point)
+    return np.linalg.norm(scaled_diff, ord=p)
 
-# Funkcja skalaryzowana dla problemu trzykryterialnego (minimalizacja odległości)
-def distance_scalarization_G(x, target_point, p=2):
+# Funkcja skalaryzowana dla problemu trzykryterialnego z uwzględnieniem wag
+def distance_scalarization_G(x, target_point, lambda_weights, p=2):
     G_values = np.array([G1(x[0], x[1], x[2]), G2(x[0], x[1], x[2]), G3(x[0], x[1], x[2])])
-    return np.linalg.norm(G_values - target_point, ord=p)
+    scaled_diff = lambda_weights * (G_values - target_point)
+    return np.linalg.norm(scaled_diff, ord=p)
 
 # Generowanie zbioru Pareto dla problemu dwukryterialnego z minimalizacją odległości
-def generate_pareto_set_F_distance(target_point, p=2):
+def generate_pareto_set_F_distance(target_point, lambda_vectors, p=2):
     pareto_points_decision = []
     pareto_points_objective = []
-    for _ in range(20):  # Liczba prób optymalizacji z różnymi punktami początkowymi
+    for lambda_weights in lambda_vectors:
         initial_guess = np.random.uniform(-1, 1, 2)
-        result = minimize(distance_scalarization_F, initial_guess, args=(target_point, p), bounds=[(-1, 1), (-1, 1)])
+        result = minimize(distance_scalarization_F, initial_guess, args=(target_point, lambda_weights, p), bounds=[(-1, 1), (-1, 1)])
         if result.success:
             pareto_points_decision.append(result.x)
             pareto_points_objective.append([F1(result.x[0], result.x[1]), F2(result.x[0], result.x[1])])
     return np.array(pareto_points_decision), np.array(pareto_points_objective)
 
 # Generowanie zbioru Pareto dla problemu trzykryterialnego z minimalizacją odległości
-def generate_pareto_set_G_distance(target_point, p=2):
+def generate_pareto_set_G_distance(target_point, lambda_vectors, p=2):
     pareto_points_decision = []
     pareto_points_objective = []
-    for _ in range(20):  # Liczba prób optymalizacji z różnymi punktami początkowymi
+    for lambda_weights in lambda_vectors:
         initial_guess = np.random.uniform(-1, 1, 3)
-        result = minimize(distance_scalarization_G, initial_guess, args=(target_point, p), bounds=[(-1, 1), (-1, 1), (-1, 1)])
+        result = minimize(distance_scalarization_G, initial_guess, args=(target_point, lambda_weights, p), bounds=[(-1, 1), (-1, 1), (-1, 1)])
         if result.success:
             pareto_points_decision.append(result.x)
             pareto_points_objective.append([G1(result.x[0], result.x[1], result.x[2]),
                                             G2(result.x[0], result.x[1], result.x[2]),
                                             G3(result.x[0], result.x[1], result.x[2])])
     return np.array(pareto_points_decision), np.array(pareto_points_objective)
+
+# Generowanie kombinacji wag dla skalaryzacji
+def generate_lambda_vectors(n, step=0.1):
+    if n == 2:
+        return [np.array([l1, 1 - l1]) for l1 in np.arange(0, 1 + step, step)]
+    elif n == 3:
+        return [np.array([l1, l2, 1 - l1 - l2]) for l1 in np.arange(0, 1 + step, step)
+                for l2 in np.arange(0, 1 - l1 + step, step) if l1 + l2 <= 1]
 
 # Wizualizacja zbioru Pareto w przestrzeni decyzyjnej 2D
 def plot_decision_space_2d(pareto_points, title):
@@ -113,14 +123,18 @@ def plot_pareto_approximation_3d(pareto_points, title):
 # Parametry: punkt dominujący i norma
 target_point_2D = np.array([0, 0])  # Punkt dominujący dla 2D
 target_point_3D = np.array([0, 0, 0])  # Punkt dominujący dla 3D
-p_norm = 2  # Norma (np. p=2 dla normy euklidesowej)
+p_norm = np.inf  # Norma Czebyszewa działa chyba najlepiej
+
+# Generowanie wektorów wag
+lambda_vectors_2D = generate_lambda_vectors(2)
+lambda_vectors_3D = generate_lambda_vectors(3)
 
 # Obliczenia i wizualizacja dla dwukryterialnego przypadku
-pareto_decision_F, pareto_objective_F = generate_pareto_set_F_distance(target_point_2D, p=p_norm)
+pareto_decision_F, pareto_objective_F = generate_pareto_set_F_distance(target_point_2D, lambda_vectors_2D, p=p_norm)
 plot_decision_space_2d(pareto_decision_F, "Zbiór P(U, F) dla problemu dwukryterialnego")
 plot_pareto_approximation_2d(pareto_objective_F, "Front Pareto FP(U) dla problemu dwukryterialnego")
 
 # Obliczenia i wizualizacja dla trzykryterialnego przypadku
-pareto_decision_G, pareto_objective_G = generate_pareto_set_G_distance(target_point_3D, p=p_norm)
+pareto_decision_G, pareto_objective_G = generate_pareto_set_G_distance(target_point_3D, lambda_vectors_3D, p=p_norm)
 plot_decision_space_3d(pareto_decision_G, "Zbiór P(U, F) dla problemu trzykryterialnego")
 plot_pareto_approximation_3d(pareto_objective_G, "Front Pareto FP(U) dla problemu trzykryterialnego")
